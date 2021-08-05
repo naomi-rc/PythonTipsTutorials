7c3b52a13b8cd7f82d6978dedd26bc3a61074f70
naomi-rc <35537740+naomi-rc@users.noreply.github.com>
2021-08-04 20:37:15 -0400

    added python c extensions summary

---
+++ python_c_extensions.py
@@ -0,0 +1,4 @@
+# Three ways : 
+#   CTypes module : easiest
+#   SWIG : adds interface, more complex, for targeting multiple languages
+#   Python/C API : most used, most complex, objects can be manipulated too, uses Python.h header (C code written specifically to interface with python)
